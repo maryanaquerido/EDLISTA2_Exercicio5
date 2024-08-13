e3daab908ade73d56e6dc995eb31904f65dbc95d
Maryana <maryana.carmo@fatec.sp.gov.br>
2024-08-12 22:14:13 -0300

    Commit inicial

---
+++ EDL2RecursividadeEx5/src/controller/ControllerMDC.java
@@ -0,0 +1,27 @@
+package controller;
+
+public class ControllerMDC {
+	public ControllerMDC () {
+		super();
+	}
+	
+	public int MDC (int x, int y) {
+		if () {
+		return 1;	
+		}
+		else 
+			if ((x % 2==0) && (y % 2==0)) {
+				
+				
+			
+			
+			
+			
+			
+			//if (x > y) {
+				
+				//MDC(x - y, y);
+			}
+	}
+
+}
